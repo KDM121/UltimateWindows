0b4c13be413afef37086c4eed0cd864a8d6dc1a5
KDM121 <117851257+KDM121@users.noreply.github.com>
2024-01-28 11:42:12 +0000

    Create UserInterface.ps1
    Create UserInterface.ps1 and folder directories

---
+++ GUI/UserInterface.ps1
@@ -0,0 +1,16 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+$FormObject = [System.Windows.Forms.Form]
+$LabelObject = [System.Windows.Forms.Label]
+
+$HelloWorldForm=New-Object $FormObject
+$HelloWorldForm.ClientSize='500,300'
+$HelloWorldForm.Text='Hello World - Tutorial'
+$HelloWorldForm.BackColor="#001633"
+
+# displays form
+$HelloWorldForm.ShowDialog()
+
+
+## cleans up form
+$HelloWorldForm.Dispose()
