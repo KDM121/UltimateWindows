daacfda20a2a7fe071243d485d36a6a23cb0790d
KDM121 <117851257+KDM121@users.noreply.github.com>
2024-01-28 12:10:29 +0000

    Update UserInterface.ps1

--- GUI/UserInterface.ps1
+++ GUI/UserInterface.ps1
@@ -2,11 +2,26 @@
 
 $FormObject = [System.Windows.Forms.Form]
 $LabelObject = [System.Windows.Forms.Label]
+$ButtonObject = [System.Windows.Forms.Button]
 
 $HelloWorldForm=New-Object $FormObject
 $HelloWorldForm.ClientSize='500,300'
 $HelloWorldForm.Text='Hello World - Tutorial'
 $HelloWorldForm.BackColor="#001633"
+
+$lbltitle=New-Object $LabelObject
+$lbltitle.Text='Hello World!'
+$lbltitle.AutoSize=$true
+$lbltitle.Font='Verdana,24,style=Bold'
+$lbltitle.Location=New-Object System.Drawing.Point(120,110)
+
+$btnHello=New-Object $ButtonObject
+$btnHello.Text='Say Hello'
+$btnHello.Autosize=$true
+$btnHello.Location=New-Object System.Drawing.Point(175,180)
+
+$HelloWorldForm.Controls.AddRange(@($lbltitle,$btnHello))
+
 
 # displays form
 $HelloWorldForm.ShowDialog()
